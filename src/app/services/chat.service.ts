b6843def2240274bac692936862ad1842b944362
tyler <tyler.reitz@protonmail.com>
2026-05-22 15:33:01 -0700

    Adding Google Authentation

--- src/app/services/chat.service.ts
+++ src/app/services/chat.service.ts
@@ -66,18 +66,32 @@ export class ChatService {
   user$ = user(this.auth);
   currentUser: User | null = this.auth.currentUser;
   userSubscription: Subscription;
-  
+
   constructor() {
     this.userSubscription = this.user$.subscribe((aUser: User | null) => {
         this.currentUser = aUser;
     });
   }
 
-  // Login Friendly Chat.
-  login() {}
+  // Signs-in Friendly Chat.
+  login() {
+    signInWithPopup(this.auth, this.provider).then((result) => {
+        const credential = GoogleAuthProvider.credentialFromResult(result);
+        this.router.navigate(['/', 'chat']);
+        return credential;
+    })
+}
 
   // Logout of Friendly Chat.
-  logout() {}
+  logout() {
+      signOut(this.auth).then(() => {
+          this.router.navigate(['/', 'login'])
+          console.log('signed out');
+      }).catch((error) => {
+          console.log('sign out error: ' + error);
+      })
+  }
+
 
   // Adds a text or image message to Cloud Firestore.
   addMessage = async (
