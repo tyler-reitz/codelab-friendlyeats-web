2bf446e9123ec442d7a4c43ed8f6acd1bbe53a02
tyler <tyler.reitz@protonmail.com>
2026-05-27 14:17:58 -0700

    13: show notifications

--- src/app/services/chat.service.ts
+++ src/app/services/chat.service.ts
@@ -192,8 +192,46 @@ export class ChatService {
   ) {
     return null;
   }
-  // Requests permissions to show notifications.
-  requestNotificationsPermissions = async () => {};
 
-  saveMessagingDeviceToken = async () => {};
+  // Requests permissions to show notifications.
+  requestNotificationsPermissions = async () => {
+      console.log('Requesting notifications permission...');
+      const permission = await Notification.requestPermission();
+
+      if (permission === 'granted') {
+        console.log('Notification permission granted.');
+        // Notification permission granted.
+        await this.saveMessagingDeviceToken();
+      } else {
+        console.log('Unable to get permission to notify.');
+      }
+  }
+
+
+  // Saves the messaging device token to Cloud Firestore.
+  saveMessagingDeviceToken= async () => {
+      try {
+        const currentToken = await getToken(this.messaging);
+        if (currentToken) {
+          console.log('Got FCM device token:', currentToken);
+          // Saving the Device Token to Cloud Firestore.
+          const tokenRef = doc(this.firestore, 'fcmTokens', currentToken);
+          await setDoc(tokenRef, { uid: this.auth.currentUser?.uid });
+
+          // This will fire when a message is received while the app is in the foreground.
+          // When the app is in the background, firebase-messaging-sw.js will receive the message instead.
+          onMessage(this.messaging, (message) => {
+            console.log(
+              'New foreground notification from Firebase Messaging!',
+              message.notification
+            );
+          });
+        } else {
+          // Need to request permissions to show notifications.
+          this.requestNotificationsPermissions();
+        }
+      } catch(error) {
+        console.error('Unable to get messaging token.', error);
+      };
+  }
 }
